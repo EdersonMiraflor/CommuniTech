0b21c9e88467ebdd10bdd38eed4c0f036d805248
coleenlustre9 <coleenlustre@gmail.com>
2024-09-04 14:54:00 +0800

    Updated branch Coleen

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,18 @@
-@extends('layouts.layout')
-@section('contents')
-    <h1 class="text-danger">index page</h1>
-    <p class="text-success">This is Home Page</p>
-    <!-- Sample Bootstrap Usage -->
-    <img class="position-absolute top-20 start-50 translate-middle" style="width: 5rem;" src="img/Manito-Logo.png" alt="Logo of Manito">
-    <!-- Proper Linking in Pages Usage -->
-    <a href="/contact" style="text-align:center;">Go to Contact</a><br>
-    <a href="/transaction" style="text-align:center;">Go to Transaction</a><br>
-    <a href="/login" style="text-align:center;">Go to Login</a>
-@endsection
 
+@extends('layouts.layout2')
+@section('content')
+
+<br><br><br><br><br>
+<h1 class="text">To Ensure You are From Manito</h1>
+    <h1 class="text">Please Select A Place that Belongs to Manito Albay</h1>
+    <div class="images">
+        <img src="img/auth-img/img1.jpg" alt="Image 1" class="image" data-name="correct">
+        <img src="img/auth-img/img2.jpg" alt="Image 2" class="image" data-name="wrong">
+        <img src="img/auth-img/img3.jpg" alt="Image 3" class="image" data-name="wrong">
+        <img src="img/auth-img/img4.jpg" alt="Image 4" class="image" data-name="wrong">
+        <img src="img/auth-img/img5.jpg" alt="Image 5" class="image" data-name="wrong">
+        <img src="img/auth-img/img6.jpg" alt="Image 6" class="image" data-name="wrong">
+    </div>
+<button class="button" id="verifyButton">Verify</button>
+<script src="js/main.js"></script>
+@endsection
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,72 +1,113 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manito Online Services</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('img/communitechlogo.png') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <title>Basic Pages</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="css/main.css">
     <style>
         .header-bar {
             background-color: #e8f7ec;
             padding: 10px;
             display: flex;
-            justify-content: center; /* Center all items within the header */
+            justify-content: center;
             align-items: center;
-            flex-wrap: wrap; /* Allow wrapping of content */
+            flex-wrap: wrap;
         }
         .header-title {
             display: flex;
             align-items: center;
             justify-content: center;
-            gap: 10px; /* Adds equal spacing between elements */
+            gap: 10px;
             text-align: center;
-            flex: 1; /* Allows header-title to take available space */
+            flex: 1;
         }
         .header-title img {
             height: 50px;
             width: auto;
         }
         @media (max-width: 576px) {
-            /* Hide date/time and user info on small screens */
             .hide-on-small {
                 display: none !important;
             }
-            /* Adjust header title on small screens */
             .header-title {
-                gap: 5px; /* Reduce gap for smaller screens */
+                gap: 5px;
             }
             .header-title img {
-                height: 40px; /* Adjust logo size for smaller screens */
+                height: 40px;
             }
         }
+        .nav-item.inline-block {
+            display: inline-block;
+            margin-right: 10px;
+        }
+        .nav-item.inline-block:last-child {
+            margin-right: 0;
+        }
     </style>
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <header class="header-bar">
-        <!-- Date and time (hidden on small screens) -->
         <div class="hide-on-small">
-            <span id="current-date-time"></span>
+            <span id="current-date-time" style="margin-left: 50px;"></span>
         </div>
-        <!-- Header title, logos, and text -->
         <div class="header-title">
-            <!-- Left Logo -->
             <img src="{{ asset('img/communitechlogo.png') }}" alt="Logo of Communitech" class="img-fluid">
-            <!-- Center Text -->
             <span>
                 Republic of the Philippines<br>
                 Province of Albay<br>
                 Municipality of Manito
             </span>
-            <!-- Right Logo -->
             <img src="{{ asset('img/manito-logo.png') }}" alt="Logo of Manito" class="img-fluid">
         </div>
-        <!-- User info (hidden on small screens) -->
         <div class="d-flex align-items-center hide-on-small">
-            <span>Logged in as User</span>
-            <i class="bi bi-person ms-2"></i> <!-- User icon -->
+            <ul class="navbar-nav ms-auto" style="margin-right: 50px; display: inline-block;">
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item inline-block">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item inline-block">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+            <i class="bi bi-person ms-2"></i>
         </div>
     </header>
 
@@ -82,6 +123,10 @@
         setInterval(updateDateTime, 1000);
         updateDateTime();
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Include Popper.js and Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz39BpwPqv92mGhFCc3C2wWwOtc1HbP5mrxG6ZAwXjC5K8tb1HBeI2xfPv" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-QTW0l0k/QwG7m8CZv/t5bO5PBWxP8MXZ3zFgLeMw6b00pYX/mbU8vcUQj9uZQe6b5" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-QTW0l0k/QwG7m8CZv/t5bO5PBWxP8MXZ3zFgLeMw6b00pYX/mbU8vcUQj9uZQe6b5" crossorigin="anonymous"></script>
 </body>
 </html>
