b7a658d6147d2aeb16c02bf8d40d112bbfddef0a
coleenlustre9 <coleenlustre@gmail.com>
2024-09-09 23:55:21 +0800

    Navigation bar

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,7 @@ Route::get('/forgot', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
