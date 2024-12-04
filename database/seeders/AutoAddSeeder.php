2f50547a035b6f754e87777e14bafe19ea95ff05
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2024-12-04 17:22:57 +0800

    Added Seeder to add primary admin
    Added Seeder to add primary admin

---
+++ database/seeders/AutoAddSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use DB;
+
+class AutoAddSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    /*Automatic add a admin user*/
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'Credential' => 'admin', 
+            'name' => 'CommuniTech4', 
+            'Middle_Name' => 'Manito', 
+            'Last_Name' => 'Civilregistar', 
+            'Birth_Date' => '2024-12-26', 
+            'Sex' => 'male', 
+            'Mobile_Number' => '09092902988',
+            'email' => 'communitech04@gmail.com', 
+            'password' => Hash::make('communitech4'), 
+            'Address' => 'Manila, Philippines', 
+            'Request_Id' => null, 
+        ]);
+    }
+}
