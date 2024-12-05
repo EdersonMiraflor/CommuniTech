cbffc1bc9ac83c607d6de8aa87c4fbbdd22eaf8c
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2024-12-05 22:44:59 +0800

    Fix Register again, this time its sure
    Fix Register again, this time its sure

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -175,15 +175,12 @@
                             </div>
                         </div>
                          <!-- Buttons -->
-<!--{{   @csrf-- }}-->
-<!--- dd($user);-->
                          <div class="row mb-0">
                             <div class="col-md-6 offset-md-3 text-center">
                                 <button type="button" class="cancel-button"> {{ __('Cancel') }} </button>
                                 <button type="submit" class="register-button">
                                     {{ __('Register') }}
                                 </button>
-<!--Waglalagyan href ang button since nasa controller-->
                             </div>
                         </div>
                     </form>
