edb1549ffcd1a8769522fcb961e4ce98139c7ab5
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2024-12-24 16:10:36 +0800

    fix column in table  which it should fit the attribute in the database
    fix column in table  which it should fit the attribute in the database

--- resources/views/displayRecords.blade.php
+++ resources/views/displayRecords.blade.php
@@ -111,13 +111,6 @@
         <th>Marriage Country</th>
         <th>Attendant Role</th>
         <th>Other Attendant Role</th>
-        <th>Father's Name</th>
-        <th>Mother's Name</th>
-        <th>Child's Name</th>
-        <th>Birth Date</th>
-        <th>Birth Place</th>
-        <th>Signature 1</th>
-        <th>Signature 2</th>
     </tr>
 </thead>
 
@@ -126,15 +119,19 @@
         <tr>
             <td>{{ $index + 1 }}</td>
             <td>{{ $record->user_name }}</td>
-            <td>{{ $record->child_middle_name }} {{ $record->child_first }}  {{ $record->child_last }}</td>
-            <td>{{ $record->mother_first_name }} {{ $record->mother_middle_name }} {{ $record->mother_last_name }}</td>
-            <td>{{ $record->father_first_name }} {{ $record->father_middle_name }} {{ $record->father_last_name }}</td>
+            <td>{{ $record->child_first }} </td>
+            <td>{{ $record->child_middle }} </td>
+            <td>{{ $record->child_last }} </td>
+            <td>{{ $record->child_sex }}</td>
             <td>{{ $record->child_birthdate }}</td>
             <td>{{ $record->child_birthplace }}</td>
             <td>{{ $record->multiple_birth }}</td>
             <td>{{ $record->birth_type }}</td>
             <td>{{ $record->birth_order }}</td>
             <td>{{ $record->birth_weight }}</td>
+            <td>{{ $record->mother_first_name }}</td>
+            <td>{{ $record->mother_middle_name }}</td>
+            <td>{{ $record->mother_last_name }}</td>
             <td>{{ $record->citizenship }}</td>
             <td>{{ $record->religion }}</td>
             <td>{{ $record->total_number }}</td>
@@ -146,7 +143,9 @@
             <td>{{ $record->mother_city }}</td>
             <td>{{ $record->mother_province }}</td>
             <td>{{ $record->mother_country }}</td>
-            <td>{{ $record->father_first_name }} {{ $record->father_middle_name }} {{ $record->father_last_name }}</td>
+            <td>{{ $record->father_first_name }}</td>
+            <td>{{ $record->father_middle_name }}</td>
+            <td>{{ $record->father_last_name }}</td>
             <td>{{ $record->citizenship2 }}</td>
             <td>{{ $record->religion2 }}</td>
             <td>{{ $record->occupation2 }}</td>
@@ -162,24 +161,19 @@
             <td>{{ $record->marriage_country }}</td>
             <td>{{ $record->attendant_role }}</td>
             <td>{{ $record->other_attendant_role }}</td>
-            <td>{{ $record->father_name }}</td>
-            <td>{{ $record->mother_name }}</td>
-            <td>{{ $record->name_child }}</td>
-            <td>{{ $record->birth_date }}</td>
-            <td>{{ $record->birth_place }}</td>
-            <td>{{ $record->signature1 }}</td>
-            <td>{{ $record->signature2 }}</td>
         </tr>
     @endforeach
 </tbody>
 
 </table>
 </div>
-        @elseif ($category == '2' && count($records) > 0)
-            <h3>Marriage Certificate Records</h3>
-            <div class="table-container">
-            <table class="table table-bordered mt-3">
-            <thead>
+
+@elseif ($category == '2' && count($records) > 0)
+    <h3>Marriage Certificate Records</h3>
+    <div class="table-container">
+    <table class="table table-bordered mt-3">
+    <thead>
+        
     <tr>
     <th>#</th>
         <th>user_name</th>
@@ -226,31 +220,6 @@
         <th>officiant_name</th>
         <th>officiant_position</th>
         <th>witnesses</th>
-        <th>affiant_name</th>
-        <th>address</th>
-        <th>marriage_registration_for</th>
-        <th>marriage_date2</th>
-        <th>ceremony_type</th>
-        <th>license_required</th>
-        <th>license_no</th>
-        <th>license_date</th>
-        <th>license_place</th>
-        <th>license_required2</th>
-        <th>article_no</th>
-        <th>citizenship</th>
-        <th>spouse_citizenship</th>
-        <th>delay_reason</th>
-        <th>day2</th>
-        <th>month2</th>
-        <th>year2</th>
-        <th>location</th>
-        <th>subscribed_day</th>
-        <th>subscribed_month</th>
-        <th>subscribed_year</th>
-        <th>notary_location</th>
-        <th>admin_officer_position</th>
-        <th>admin_officer_name</th>
-        <th>admin_officer_address</th>
     </tr>
 </thead>
 
@@ -259,76 +228,49 @@
         <tr>
         <td>{{ $index + 1 }}</td>
             <td>{{ $record->user_name }}</td>
-            <td>{{ $record->husband_first_name }} </td>
+            <td>{{ $record->husband_first_name}} </td>
             <td>{{ $record->husband_middle_name }}</td>
             <td>{{ $record->husband_last_name }}</td>
             <td>{{ $record->husband_birthdate }}</td>
             <td>{{ $record->husband_age }}</td>
             <td>{{ $record->husband_city_municipality }}</td>
-            <td>{{ $record->multiple_birth }}</td>
-            <td>{{ $record->husband_province }} </td>
-            <td>{{ $record->husband_country }}</td>
+            <td>{{ $record->husband_province }}</td>
+            <td>{{ $record->husband_country }} </td>
             <td>{{ $record->husband_citizenship }}</td>
             <td>{{ $record->husband_residence }}</td>
             <td>{{ $record->husband_religion }}</td>
             <td>{{ $record->husband_father_first_name }}</td>
-            <td>{{ $record->husband_father_middle_name }} </td>
-            <td>{{ $record->husband_father_last_name }}</td>
+            <td>{{ $record->husband_father_middle_name }}</td>
+            <td>{{ $record->husband_father_last_name }} </td>
             <td>{{ $record->husband_father_citizenship }}</td>
             <td>{{ $record->husband_mother_first_name }}</td>
             <td>{{ $record->husband_mother_middle_name }}</td>
             <td>{{ $record->husband_mother_maiden_last_name }}</td>
-            <td>{{ $record->husband_mother_citizenship }} </td>
-            <td>{{ $record->wife_first_name }}</td>
+            <td>{{ $record->husband_mother_citizenship }}</td>
+            <td>{{ $record->wife_first_name }} </td>
             <td>{{ $record->wife_middle_name }}</td>
             <td>{{ $record->wife_last_name }}</td>
             <td>{{ $record->wife_birthdate }}</td>
             <td>{{ $record->wife_age }}</td>
-            <td>{{ $record->wife_city_municipality }} </td>
-            <td>{{ $record->wife_province }}</td>
+            <td>{{ $record->wife_city_municipality }}</td>
+            <td>{{ $record->wife_province }} </td>
             <td>{{ $record->wife_country }}</td>
             <td>{{ $record->wife_citizenship }}</td>
             <td>{{ $record->wife_residence }}</td>
-
             <td>{{ $record->wife_religion }}</td>
             <td>{{ $record->wife_father_first_name }}</td>
-            <td>{{ $record->wife_father_middle_name }} </td>
-            <td>{{ $record->wife_father_last_name }}</td>
+            <td>{{ $record->wife_father_middle_name }}</td>
+            <td>{{ $record->wife_father_last_name }} </td>
             <td>{{ $record->wife_father_citizenship }}</td>
             <td>{{ $record->wife_mother_first_name }}</td>
             <td>{{ $record->wife_mother_middle_name }}</td>
             <td>{{ $record->wife_mother_maiden_last_name }}</td>
-            <td>{{ $record->wife_mother_citizenship }} </td>
-            <td>{{ $record->marriage_date1 }}</td>
+            <td>{{ $record->wife_mother_citizenship }}</td>
+            <td>{{ $record->marriage_date1 }} </td>
             <td>{{ $record->marriage_place }}</td>
             <td>{{ $record->officiant_name }}</td>
             <td>{{ $record->officiant_position }}</td>
             <td>{{ $record->witnesses }}</td>
-            <td>{{ $record->affiant_name }}</td>
-            <td>{{ $record->address }} </td>
-            <td>{{ $record->marriage_registration_for }}</td>
-            <td>{{ $record->marriage_date2 }}</td>
-            <td>{{ $record->ceremony_type }}</td>
-            <td>{{ $record->license_required }}</td>
-            <td>{{ $record->license_no }}</td>
-            <td>{{ $record->license_date }} </td>
-            <td>{{ $record->license_place }}</td>
-            <td>{{ $record->license_required2 }}</td>
-            <td>{{ $record->article_no }}</td>
-            <td>{{ $record->citizenship }}</td>
-            <td>{{ $record->spouse_citizenship }}</td>
-            <td>{{ $record->delay_reason }}</td>
-            <td>{{ $record->day2 }} </td>
-            <td>{{ $record->month2 }}</td>
-            <td>{{ $record->year2 }}</td>
-            <td>{{ $record->location }}</td>
-            <td>{{ $record->subscribed_day }}</td>
-            <td>{{ $record->subscribed_month }}</td>
-            <td>{{ $record->subscribed_year }} </td>
-            <td>{{ $record->notary_location }}</td>
-            <td>{{ $record->admin_officer_position }}</td>
-            <td>{{ $record->admin_officer_name }}</td>
-            <td>{{ $record->admin_officer_address }}</td>
         </tr>
     @endforeach
 </tbody>
@@ -343,49 +285,40 @@
         <th>#</th>
         <th>user_name</th>f
         <th>Full Name</th>
+        <th>Sex</th>
         <th>Date of Death</th>
-        <th>Place of Death</th>
-        <th>Cause of Death</th>
-        <th>Place of Occurrence</th>
-        <th>Immediate Cause</th>
-        <th>Antecedent Cause</th>
-        <th>Underlying Cause</th>
-        <th>Other Conditions</th>
-        <th>Maternal Condition</th>
-        <th>Manner of Death</th>
-        <th>Autopsy</th>
-        <th>Type of Attendant</th>
-        <th>Attendance Duration</th>
-        <th>Certifying Officer</th>
-        <th>Certification Date</th>
-        <th>Corpse Disposal Method</th>
-        <th>Other Disposal Method Specify</th>
-        <th>Cemetery or Crematory Name</th>
-        <th>Cemetery or Crematory Address</th>
-        <th>Age of Mother</th>
-        <th>Method of Delivery</th>
-        <th>Length of Pregnancy</th>
-        <th>Type of Birth</th>
-        <th>Multiple Birth Position</th>
-        <th>Affiant Name</th>
-        <th>Legal Status</th>
-        <th>Affiant Address</th>
-        <th>Deceased Name</th>
-        <th>Burial Place</th>
-        <th>Attended By</th>
-        <th>Attended By Person</th>
-        <th>Not Attended</th>
-        <th>Reason for Delay</th>
-        <th>Day Signed</th>
-        <th>Month Signed</th>
-        <th>Year Signed</th>
-        <th>Place Signed</th>
-        <th>Day Sworn</th>
-        <th>Month Sworn</th>
-        <th>Year Sworn</th>
-        <th>Place Sworn</th>
-        <th>Tax Cert Date</th>
-        <th>Tax Cert Place</th>
+        <th>date_of_birth</th>
+        <th>completed_years</th>
+        <th>months_days</th>
+        <th>hours_minutes_seconds</th>
+        <th>place_of_death</th>
+        <th>civil_status</th>
+        <th>religion</th>
+        <th>citizenship</th>
+        <th>residence</th>
+        <th>father_name</th>
+        <th>mother_maiden_name</th>
+        <th>immediate_cause</th>
+        <th>antecedent_cause</th>
+        <th>underlying_cause</th>
+        <th>other_conditions</th>
+        <th>maternal_condition</th>
+        <th>manner_of_death</th>
+        <th>place_of_occurrence</th>
+        <th>autopsy</th>
+        <th>type_of_attendant</th>
+        <th>attendance_duration</th>
+        <th>certifying_officer</th>
+        <th>certification_date</th>
+        <th>corpse_disposal_method</th>
+        <th>other_disposal_method_specify</th>
+        <th>cemetery_or_crematory_name</th>
+        <th>cemetery_or_crematory_address</th>
+        <th>age_of_mother</th>
+        <th>method_of_delivery</th>
+        <th>length_of_pregnancy</th>
+        <th>type_of_birth</th>
+        <th>multiple_birth_position</th>
     </tr>
 </thead>
 
@@ -395,16 +328,26 @@
             <td>{{ $index + 1 }}</td>
             <td>{{ $record->user_name }}</td>
             <td>{{ $record->full_name }}</td>
+            <td>{{ $record->sex }}</td>
             <td>{{ $record->date_of_death }}</td>
+            <td>{{ $record->date_of_birth }}</td>
+            <td>{{ $record->completed_years }}</td>
+            <td>{{ $record->months_days }}</td>
+            <td>{{ $record->hours_minutes_seconds }}</td>
             <td>{{ $record->place_of_death }}</td>
-            <td>{{ $record->cause_of_death }}</td>
-            <td>{{ $record->place_of_occurrence }}</td>
+            <td>{{ $record->civil_status }}</td>
+            <td>{{ $record->religion }}</td>
+            <td>{{ $record->citizenship }}</td>
+            <td>{{ $record->residence }}</td>
+            <td>{{ $record->father_name }}</td>
+            <td>{{ $record->mother_maiden_name }}</td>
             <td>{{ $record->immediate_cause }}</td>
             <td>{{ $record->antecedent_cause }}</td>
             <td>{{ $record->underlying_cause }}</td>
             <td>{{ $record->other_conditions }}</td>
             <td>{{ $record->maternal_condition }}</td>
             <td>{{ $record->manner_of_death }}</td>
+            <td>{{ $record->place_of_occurrence }}</td>
             <td>{{ $record->autopsy }}</td>
             <td>{{ $record->type_of_attendant }}</td>
             <td>{{ $record->attendance_duration }}</td>
@@ -416,28 +359,9 @@
             <td>{{ $record->cemetery_or_crematory_address }}</td>
             <td>{{ $record->age_of_mother }}</td>
             <td>{{ $record->method_of_delivery }}</td>
-            <td>{{ $record->length_of_pregnancy }}</td>
+            <td>{{ $record->length_of_pregnancy }}</td>  
             <td>{{ $record->type_of_birth }}</td>
             <td>{{ $record->multiple_birth_position }}</td>
-            <td>{{ $record->affiant_name }}</td>
-            <td>{{ $record->legal_status }}</td>
-            <td>{{ $record->affiant_address }}</td>
-            <td>{{ $record->deceased_name }}</td>
-            <td>{{ $record->burial_place }}</td>
-            <td>{{ $record->attended_by }}</td>
-            <td>{{ $record->attended_by_person }}</td>
-            <td>{{ $record->not_attended }}</td>
-            <td>{{ $record->reason_delay }}</td>
-            <td>{{ $record->day_signed }}</td>
-            <td>{{ $record->month_signed }}</td>
-            <td>{{ $record->year_signed }}</td>
-            <td>{{ $record->place_signed }}</td>
-            <td>{{ $record->day_sworn }}</td>
-            <td>{{ $record->month_sworn }}</td>
-            <td>{{ $record->year_sworn }}</td>
-            <td>{{ $record->place_sworn }}</td>
-            <td>{{ $record->tax_cert_date }}</td>
-            <td>{{ $record->tax_cert_place }}</td>
         </tr>
     @endforeach
 </tbody>
